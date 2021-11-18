14261d05a5aa85d43eb5b94777b89d667f2b2458
Vincent Demeester <vdemeest@redhat.com>
2021-11-18 17:16:19 +0100

    Add scripts/debug-tmux.sh script
    This script starts a debug tmux session based on a KUBECONFIG. As of
    today it sets the KUBECONFIG environment based on the given name (it
    makes some assumption there that we might want to refine in the
    future) and starts kail on the tekton-pipelines or openshift-pipelines
    namespace.
    
    Next step will be to add subcommands for more helpers
    - Look into openshift-operators too (in case of OpenShift)
    - Watch pods in a namespace (in a Pane)
    - Fire and watch a TaskRun/PipelineRun (in a Pane)
    - …
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

---
+++ scripts/debug-tmux.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# This script start a TMUX session targeting a cluster to debug it
+KUBECONFIG_NAME=$1
+
+set -eu -o pipefail
+
+#SESSION_NAME=${KUBECONFIG_NAME}
+SESSION_NAME=$(echo ${KUBECONFIG_NAME} | tr '.' '_' )
+
+KUBECONFIG=${HOME}/.kube/${KUBECONFIG_NAME}
+
+# Guess Namespace
+# FIXME: this is ugly
+echo "Detecting…"
+TEKTON_NAMESPACE=""
+kubectl --kubeconfig="${KUBECONFIG}" get namespace | grep "tekton-pipelines" && {
+    echo "  Running in kubernetes, tekton-pipelines namespace"
+    TEKTON_NAMESPACE="tekton-pipelines"
+} || {
+    echo -n ""
+}
+kubectl --kubeconfig="${KUBECONFIG}" get namespace | grep "openshift-pipelines" && {
+    echo "  Running in kubernetes, tekton-pipelines namespace"
+    TEKTON_NAMESPACE="openshift-pipelines"
+} || {
+    echo -n ""
+}
+if [[ $TEKTON_NAMESPACE == "" ]]; then
+    echo "Tekton is not present in the cluster, nothing to debug…"
+    exit 1
+fi
+
+# FIXME: add helpers like
+# - watch tekton resource in the namespace
+# - watch pods in this namespace, …
+
+echo "Creating a session ${SESSION_NAME}…"
+tmux new-session -d -s ${SESSION_NAME} -n main
+tmux setenv -t ${SESSION_NAME} KUBECONFIG ${KUBECONFIG}
+tmux send-keys -t ${SESSION_NAME}:main "export KUBECONFIG=${KUBECONFIG}" C-m
+tmux send-keys -t ${SESSION_NAME}:main "tmux new-window -n controllers-logs kail --since=1h --ns=${TEKTON_NAMESPACE}" C-m
+tmux send-keys -t ${SESSION_NAME}:main "tmux select-window -t main" C-m
+tmux send-keys -t ${SESSION_NAME}:main C-l
+
+echo "Attaching the session…"
+tmux attach-session -t ${SESSION_NAME}
